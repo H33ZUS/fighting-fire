785ddd5c6d31706e973318e2c26fd53fd6dfcfd8
Tobias Olloz <olloz@student.chalmers.se>
2025-10-02 14:18:16 +0200

    #4 Add console view of the simulation

--- src/main.go
+++ src/main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"time"
+
+	"github.com/gosuri/uilive"
 )
 
 type Cell struct {
@@ -15,6 +17,9 @@ type Cell struct {
 var grid [][]Cell
 
 func main() {
+
+	consoleWriter := uilive.New()
+	consoleWriter.Start()
 
 	gridSize := 20
 	grid = make([][]Cell, gridSize)
@@ -32,10 +37,10 @@ func main() {
 	fireManager := NewFireManager(&grid, gridSize)
 	fireManager.Start(ctx) // starts spawn goroutine, fire spawns every 5 sec
 
-	ticker := time.NewTicker(2 * time.Second)
+	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()
 
-	timeout := time.After(30 * time.Second) // channel that signals after 30 sec
+	timeout := time.After(60 * time.Second) // channel that signals after 30 sec
 
 	for {
 		select {
@@ -45,7 +50,7 @@ func main() {
 			time.Sleep(500 * time.Millisecond) // give time for clean up
 			return
 
-		case <-ticker.C: // counts fires every 2 sec
+		case <-ticker.C: // Updating the scene based on ticker interval
 			count := 0
 			for i := 0; i < gridSize; i++ {
 				for j := 0; j < gridSize; j++ {
@@ -54,7 +59,29 @@ func main() {
 					}
 				}
 			}
-			fmt.Printf("Active fires %d\n ", count)
+
+			//Draws the state of the simulation
+			size := len(grid)
+			var state string
+
+			for i := 0; i < size; i++ {
+				for j := 0; j < size; j++ {
+					var cell string
+					if grid[i][j].hasFire {
+						cell = "\033[31m🔥 \033[0m"
+					} else if grid[i][j].hasTruck {
+						cell = "🚒 "
+					} else {
+						cell = "\033[32m🌲 \033[0m"
+					}
+					state += cell
+				}
+				state += "\n"
+			}
+
+			fmt.Fprintf(consoleWriter, "\n")
+			fmt.Fprintf(consoleWriter, "%s", state)
+			fmt.Fprintf(consoleWriter, "Active fires %d\n ", count)
 
 		}
 	}
