3451b055d50ab2f6a0838174f09c9a2bbcbc4a67
Elias Räisänen-Lundqvist <gusraisel@student.gu.se>
2025-10-03 12:46:26 +0200

    #8 Add fire growth and spread

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 type Cell struct {
-	hasFire   bool
+	HasFire   bool
 	hasTruck  bool
 	intensity int
 }
@@ -59,7 +59,7 @@ func main() {
 			count := 0
 			for i := 0; i < gridSize; i++ {
 				for j := 0; j < gridSize; j++ {
-					if grid[i][j].hasFire {
+					if grid[i][j].HasFire {
 						count++
 					}
 				}
@@ -72,7 +72,7 @@ func main() {
 			for i := 0; i < size; i++ {
 				for j := 0; j < size; j++ {
 					var cell string
-					if grid[i][j].hasFire {
+					if grid[i][j].HasFire {
 						cell = "\033[31m🔥 \033[0m"
 					} else if grid[i][j].hasTruck {
 						cell = "🚒 "
